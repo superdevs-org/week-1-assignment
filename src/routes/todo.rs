050f5691dd2a0703b9ff19c9625e2e2896091e05
keshav joshi <keshavjoshi@keshavs-MacBook-Pro.local>
2025-07-20 01:58:17 +0530

    Add Trait for pascalcase

--- src/routes/todo.rs
+++ src/routes/todo.rs
@@ -5,6 +5,7 @@ use actix_web::{HttpResponse, Responder, get, post};
 use serde::{Serialize, Deserialize};
 use crate::middleware::UserId;
 use crate::db::Db;
+use crate::routes::utils::TransformJsonKeys;
 use serde_json::{Map, Value};
 
 #[derive(Serialize, Deserialize)]
@@ -22,34 +23,10 @@ struct GetTodosResponse {
     todos: Vec<Value>
 }
 
-fn to_pascal_case(s: &str) -> String {
-    s.split('_')
-        .map(|word| {
-            let mut chars = word.chars();
-            match chars.next() {
-                Some(first) => first.to_uppercase().collect::<String>() + chars.as_str(),
-                None => "".to_string(),
-            }
-        })
-        .collect::<String>()
-}
-
 #[post("/todo")]
 pub async fn create_todo(user_id: UserId, db: Data<Arc<Mutex<Db>>>, body: Json<CreateTodoRequest>) -> impl Responder {
-    let input = body.text.clone();
 
-    let mut transformed = Map::new();
-
-    if let Some(obj) = input.as_object() {
-        for (key, value) in obj {
-            let pascal_key = to_pascal_case(key);
-
-            let new_key = format!("TodoApp{}", pascal_key);
-            transformed.insert(new_key, value.clone());
-        }
-    }
-
-    let todo = Value::Object(transformed);
+    let todo = body.text.clone();
     
     let mut db = db.lock().unwrap();
     db.create_todo(user_id.0, todo);
@@ -62,7 +39,9 @@ pub async fn create_todo(user_id: UserId, db: Data<Arc<Mutex<Db>>>, body: Json<C
 pub async fn get_todos(user_id: UserId, db: Data<Arc<Mutex<Db>>>) -> impl Responder {
     let db = db.lock().unwrap();
     let todos = db.get_todos(user_id.0);
+
+    let transformed: Vec<Value> = todos.into_iter().map(|v| v.transform_keys("TodoApp")).collect();
     HttpResponse::Ok().json(GetTodosResponse {
-        todos
+        todos:transformed
     })
 }
